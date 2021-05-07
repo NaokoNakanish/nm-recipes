66321a852ca24b07a4f9cf03153d4b59a0c83073
naokonakanish <fu2fuka.nao@gmail.com>
2021-05-07 11:32:32 +0900

    first commit

---
+++ src/views/RecipeList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>RecipeList</h1>
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+  </div>
+</template>
+
+<script>
+import RecipeCard from "@/components/RecipeCard.vue";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    RecipeCard,
+  },
+  created() {
+    this.$store.dispatch("fetchRecipes"); // データ持ってくる
+  },
+  computed: mapState(["recipes"]),
+};
+</script>
