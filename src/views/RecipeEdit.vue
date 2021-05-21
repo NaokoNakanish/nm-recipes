0b37856ce0c84023cc3654f6261018f12f3d6ad0
naokonakanish <fu2fuka.nao@gmail.com>
2021-05-21 16:07:25 +0900

    add-form

--- src/views/RecipeEdit.vue
+++ src/views/RecipeEdit.vue
@@ -1,10 +1,8 @@
 // Bootstrap使って書いてみよう
 <template>
   <b-container fluid>
-    <h1>RecipeEdit</h1>
+    <h1>Note New Recipe</h1>
     <b-form-group
-      label-cols-lg="3"
-      label="Register New Recipes"
       label-size="lg"
       label-class="font-weight-bold pt-0"
       class="mb-0"
@@ -27,12 +25,30 @@
         <b-form-input id="id"></b-form-input>
       </b-form-group>
       <b-form-group
-        label="ingredients:"
+        label="Ingredients:"
         label-for="ingredients"
         label-cols-sm="3"
         label-align-sm="right"
       >
-        <b-form-input id="ingredients"></b-form-input>
+        <b-form inline>
+          <ol v-for="ingredient in ingredients" :key="ingredient.id">
+            <label>ingredients:</label>
+            <b-form-input id="ingredients"></b-form-input>
+            <label class="quantity" for="ingredients-quantity">quantity:</label>
+            <b-form-input id="quantity"></b-form-input>
+          </ol>
+        </b-form>
+      </b-form-group>
+      <b-form-group
+        label="process:"
+        label-for="process"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <ol v-for="process in processes" :key="process.id">
+          <label>{{ process.id }}</label>
+          <b-form-input id="process"></b-form-input>
+        </ol>
       </b-form-group>
     </b-form-group>
   </b-container>
@@ -43,6 +59,8 @@ export default {
   data() {
     return {
       types: ["id", "title"],
+      ingredients: 3, // 仮置
+      processes: 3, // 仮置
     };
   },
 };
