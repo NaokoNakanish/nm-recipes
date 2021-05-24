1a6575b256ed3fdd230945cb6bfad9d11ee42dd3
naokonakanish <fu2fuka.nao@gmail.com>
2021-05-24 18:21:27 +0900

    scss-ugokann

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-// import "./app.scss";
+//import "./app.scss";
 
 Vue.config.productionTip = false;
 
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,23 @@
 const path = require("path");
 
+// https://webpack.js.org/concepts/
+// module.exports = {
+//   entry: "./path/to/my/entry/file.js",
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//     filename: "my-first-webpack.bundle.js",
+//   },
+// };
+
+// https://bootstrap-vue.org/docs
 module.exports = {
-  entry: "./path/to/my/entry/file.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "my-first-webpack.bundle.js",
+  chainWebpack: (config) => {
+    config.resolve.alias.set(
+      "vue$",
+      // If using the runtime only build
+      path.resolve(__dirname, "node_modules/vue/dist/vue.runtime.esm.js")
+      // Or if using full build of Vue (runtime + compiler)
+      // path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+    );
   },
 };
